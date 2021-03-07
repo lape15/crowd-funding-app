492a19051c5c24e357e51f3356ae6a83edce4af8
Lape15 <larape1993@gmail.com>
2021-03-07 11:58:07 +0100

    components added

---
+++ src/component/layout.jsx
@@ -0,0 +1,21 @@
+import Board from "./board";
+import TopSection from "./top-section";
+
+const HomePage = () => (
+  <div>
+    <TopSection />
+    {/* Selected pledge start */}
+    <Board/>
+    Enter your pledge $25 Continue Black Edition Stand Pledge $75 or more You
+    get a Black Special Edition computer stand and a personal thank you. You’ll
+    be added to our Backer member list. Shipping is included. 64 left Enter your
+    pledge $75 Continue Mahogany Special Edition Pledge $200 or more You get two
+    Special Edition Mahogany stands, a Backer T-Shirt, and a personal thank you.
+    You’ll be added to our Backer member list. Shipping is included. 0 left
+    Enter your pledge $200 Continue Thanks for your support! Your pledge brings
+    us one step closer to sharing Mastercraft Bamboo Monitor Riser worldwide.
+    You will get an email once our campaign is completed. Got it!
+  </div>
+);
+
+export default HomePage;
---
+++ src/component/top-section.jsx
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+import logo from '../assets/logo.svg'
+import background from '../assets/image-hero-desktop.jpg'
+
+const TopNav = styled.section`
+  display: flex;
+  justify-content:center;
+  height: 50vh;
+  background-image: url(${background});
+  background-repeat: no-repeat;
+  padding: 40px 80px;
+  background-size:  100%;
+`;
+
+const Nav = styled.nav`
+ flex:60%;
+ ${({right}) => right &&`
+    flex: 40%;
+    display: flex;
+    justify-content: flex-end;
+ `}
+`;
+
+const NavItem = styled.div`
+color: #fff;
+font-family:'Commissioner', sans-serif;
+font-weight: 400;
+font-size: 0.9rem;
+margin-right: 10px;
+`;
+
+const Logo = styled.img`
+    width: 100px;
+`
+const TopSection = () => (
+  <TopNav>
+  <Nav>
+  <NavItem>
+    <Logo src={logo} alt="logo"/>
+  </NavItem>
+  </Nav>
+    <Nav right>
+      <NavItem> About</NavItem>
+      <NavItem>Discover</NavItem>
+      <NavItem> Get Started</NavItem>
+    </Nav>
+  </TopNav>
+);
+
+export default TopSection;
